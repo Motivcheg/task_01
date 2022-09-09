e23ff5c11d1fd99f93ac00c459692e39a69e9ca4
Max <lomanuy@gmail.com>
2022-09-09 16:19:30 +0300

    for push x2

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -1,25 +1,25 @@
 ﻿double x1, x2, y1, y2, z1, z2;
 
-Console.WriteLine("Enter x1 coordinate of first point");
+Console.WriteLine("Enter x1 coordinate");
 
 x1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter y1 coordinate of first point");
+Console.WriteLine("Enter y1 coordinate");
 
 y1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter z1 coordinate of first point");
+Console.WriteLine("Enter z1 coordinate");
 
 z1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter x2 coordinate of first point");
+Console.WriteLine("Enter x2 coordinate");
 
 x2 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter y2 coordinate of second point");
+Console.WriteLine("Enter y2 coordinate");
 
 y2 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter z2 coordinate of second point");
+Console.WriteLine("Enter z2 coordinate");
 
 z2 = double.Parse(Console.ReadLine());
 
